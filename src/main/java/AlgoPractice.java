56b8ec050ab3f8a09e23abb109f3b88d32140613
RYMND <chrysanthbuster@gmail.com>
2024-11-14 16:43:19 -0500

    update

--- src/main/java/AlgoPractice.java
+++ src/main/java/AlgoPractice.java
@@ -1,20 +1,8 @@
-import javax.swing.tree.TreeNode;
 import java.util.*;
-
-// binary tree
-class Tree<T> {
-    Tree(T x) {
-        value = x;
-    }
-    T value;
-    Tree<T> left;
-    Tree<T> right;
-}
 
 public class AlgoPractice {
 
     // useful things because i dont wanna write python/javascript
-
     // Hello world
     /*
         public class main {
@@ -25,183 +13,52 @@ public class AlgoPractice {
      */
 
     public static void main(String[] args) {
-        Tree root = new Tree(1);
-        root.left = new Tree(7);
-        root.left.left = new Tree(3);
-        root.left.right = new Tree(6);
-        root.right = new Tree(5);
-        root.right.left = new Tree(2);
-        root.right.right = new Tree(4);
+        int[][] array = new int[5][5];
 
-        // String[] result = printBinaryTree(null);
-        //        for (String s: result) {
-        //            System.out.print(s + " ");
-        //        }
-
-        // print(tree) => [1, 7, 5, 3, 6, 2, 4]
-        Integer[] result = print(root);
-        for (Integer s: result) {
-            System.out.print(s + " ");
-        }
-    }
-
-    // postorder, can do without a stack
-    public static Integer[] print(Tree<Integer> tr) {
-        // assume tr not null
-        Stack<Tree<Integer>> st = new Stack<>();
-        ArrayList<Integer> al = new ArrayList<>();
-
-        st.push(tr);
-
-        while (!st.isEmpty()) {
-            Tree<Integer> top = st.peek();
-            if (top != null) {
-                System.out.println("peek " + top.value);
-                // if top is a leaf dont push null into stack
-                if (top.right != null) {
-                    System.out.println("push " + top.right.value);
-                    st.push(top.right);
-                }
-
-                if (top.left != null) {
-                    System.out.println("push " + top.left.value);
-                    st.push(top.left);
-                }
-
-                if (top.right == null && top.left == null) {
-                    System.out.println("print " + top.value);
-                    al.add(top.value);
-                    System.out.println("pop " + top.value);
-                    st.pop();
-                }
-            }
-
-        }
-
-        return al.toArray(al.toArray(new Integer[0]));
-    }
-
-    // inorder, can do without a stack
-    public static Integer[] printInorder(Tree<Integer> tr) {
-        // assume tr not null
-        Stack<Tree<Integer>> st = new Stack<>();
-        ArrayList<Integer> al = new ArrayList<>();
-
-        return al.toArray(al.toArray(new Integer[0]));
-    }
-
-    // preorder, can do without a stack
-    public static Integer[] printPreorder(Tree<Integer> tr) {
-        // assume tr not null
-        Stack<Tree<Integer>> st = new Stack<>();
-        ArrayList<Integer> al = new ArrayList<>();
-
-        st.push(tr);
-        while (!st.isEmpty()) {
-            Tree<Integer> top = st.pop();
-
-            if (top != null) {
-                al.add(top.value);
-                st.push(top.right);
-                st.push(top.left);
-            }
-
-        }
-
-        return al.toArray(new Integer[0]);
-    }
-
-    public static Integer[] printBFS(Tree<Integer> tr) {
-        // assume tr not null
-        Queue<Tree<Integer>> q = new LinkedList<>();
-        ArrayList<Integer> al = new ArrayList<>();
-        q.offer(tr);
-
-        while (!q.isEmpty()) {
-            Tree<Integer> first = q.poll();
-
-            if (first != null) {
-                al.add(first.value);
-
-                q.offer(first.left);
-                q.offer(first.right);
+        // init the array from 1 to 25 (inclusive)
+        int counter = 1;
+        for (int i =0; i < array.length; i++) {
+            for (int j =0; j < array[i].length; j++) {
+                array[i][j] = counter++;
             }
         }
 
-        return al.toArray(al.toArray(new Integer[0]));
+        print1DArray(flatten(array));
     }
 
-    // print binary tree by level and seperated by a | operator
-    // has placeholder for null nodes
-    // BFS on binary tree
-    // can reuse this pattern for level sum
-    public static String[] printBinaryTree(Tree<Integer> tree) {
-        // use bfs to print the tree
-        Queue<Tree<Integer>> q = new LinkedList<>();
-        ArrayList<String> al = new ArrayList<>();
-
-        q.offer(tree);
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
-            boolean hasNonNullNode = false;
-
-            for (int i = 0; i < levelSize; i++) {
-                Tree<Integer> node = q.poll();
-
-                if (node != null) {
-                    al.add(node.value.toString());
-
-                    // Enqueue left and right children, even if they are null
-                    q.offer(node.left);
-                    q.offer(node.right);
-
-                    // Check if there's at least one non-null child
-                    if (node.left != null || node.right != null) {
-                        hasNonNullNode = true;
-                    }
-                } else {
-                    al.add("∅");
-                    // Enqueue null placeholders for left and right to keep tree structure
-                    q.offer(null);
-                    q.offer(null);
-                }
+    // print entire 2d array
+    public static void print2DArray(int[][] array) {
+        for (int i =0; i < array.length; i++) {
+            for (int j =0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
             }
-
-            // If there are no non-null nodes at the next level, stop the traversal
-            if (!hasNonNullNode) {
-                break;
-            }
-
-            al.add("|");
+            System.out.println();
         }
-
-        return al.toArray(String[]::new);
-
     }
 
-    // no direct conversion from List<Integer> to int[]
-    public int[] arrayListToStaticPrimitiveArray(List<Integer> result) {
-        return result.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    // convert any dynamic array to static array
-    // alternatively al.toArray(new Integer[0])
-    public Integer[] arrayListToStaticArray(List<Integer> result) {
-        return result.toArray(Integer[]::new);
-    }
-
-    // for debugging
-    // print a static array
-    public void printIntArray(int[] arr) {
-        for (int i : arr) {
+    // print entire 1d array
+    public static void print1DArray(int[] array) {
+        for (int i : array) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
 
+    // array need to contain at least one element
+    public static int[] flatten(int[][] array) {
+        int[] result = new int[array.length*array[0].length];
+        int counter = 0;
+        for (int i =0; i < array.length; i++) {
+            for (int j =0; j < array[i].length; j++) {
+                result[counter++] = array[i][j];
+            }
+        }
+
+        return result;
+    }
+
     // print an arraylist
     // print a linked list
-    // print a binary tree
     // print a stack
     // print a queue
 }
---
+++ src/main/java/Array.java
@@ -0,0 +1,60 @@
+import java.util.List;
+
+public class Array {
+
+    // no direct conversion from List<Integer> to int[]
+    public int[] arrayListToStaticPrimitiveArray(List<Integer> result) {
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    // convert any dynamic array to static array
+    // alternatively al.toArray(new Integer[0])
+    public Integer[] arrayListToStaticArray(List<Integer> result) {
+        return result.toArray(Integer[]::new);
+    }
+
+    // binary search, return index of found, or -1 if not found
+    public static int basicBinarySearch(int[] sortedArray, int target) {
+        // assume array is sorted
+        int high = sortedArray.length - 1;
+        int low = 0;
+        while (low <= high) {
+            // (high - low) / 2 + low; Avoid overflow
+            int mid = (low + high) >>> 1;
+            if (sortedArray[mid] == target) {
+                return mid;
+            } else {
+                if (sortedArray[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // selection sort, move smallest to the LHS every pass
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+
+    // bubble sort, float the largest element to RHS
+    public static void bubbleSort(int[] array) {
+
+    }
+
+    // insertion sort
+}
